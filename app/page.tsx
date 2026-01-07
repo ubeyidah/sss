4c50961a05ca52dc812d0c764b06d28060dfdd18
Ubeyidah <ubeyidah@gmail.com>
2026-01-07 08:44:50 +0300

    chore: init commit

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Page() {
+  return <div className="flex items-center justify-center h-screen">
+    <h1 className="font-bold text-7xl font-mono text-muted-foreground/50">SSS</h1>
+  </div>
+}
